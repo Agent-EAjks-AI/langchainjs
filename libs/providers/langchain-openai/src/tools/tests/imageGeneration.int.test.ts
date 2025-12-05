437d9771f8c9eda5cf1452410125713d222141ec
Christian Bromann <git@bromann.dev>
2025-12-05 12:53:48 -0800

    fix test

--- libs/providers/langchain-openai/src/tools/tests/imageGeneration.int.test.ts
+++ libs/providers/langchain-openai/src/tools/tests/imageGeneration.int.test.ts
@@ -1,12 +1,16 @@
-import { expect, it, describe, test } from "vitest";
-import {
-  HumanMessage,
-  AIMessage,
-  AIMessageChunk,
-} from "@langchain/core/messages";
+import { expect, it, describe } from "vitest";
 
 import { tools } from "../index.js";
 import { ChatOpenAI } from "../../chat_models/index.js";
+
+/**
+ * We don't explictly provide types for this yet
+ */
+interface ImageGenerationToolOutput {
+  type: string;
+  result: string;
+  size: string;
+}
 
 describe("OpenAI Image Generation Tool Integration Tests", () => {
   it("imageGeneration generates an image with basic options", async () => {
@@ -26,16 +30,29 @@ describe("OpenAI Image Generation Tool Integration Tests", () => {
       "Draw the word 'TEST' in blue font on white background."
     );
     expect(response.additional_kwargs.tool_outputs).toBeDefined();
-    expect(response.additional_kwargs.tool_outputs.length).toBe(1);
-    expect(response.additional_kwargs.tool_outputs[0].type).toBe(
-      "image_generation_call"
-    );
-    expect(response.additional_kwargs.tool_outputs[0].result).toBeDefined();
     expect(
-      response.additional_kwargs.tool_outputs[0].result.length
+      (response.additional_kwargs.tool_outputs as ImageGenerationToolOutput[])
+        .length
+    ).toBe(1);
+    expect(
+      (
+        response.additional_kwargs.tool_outputs as ImageGenerationToolOutput[]
+      )[0].type
+    ).toBe("image_generation_call");
+    expect(
+      (
+        response.additional_kwargs.tool_outputs as ImageGenerationToolOutput[]
+      )[0].result
+    ).toBeDefined();
+    expect(
+      (
+        response.additional_kwargs.tool_outputs as ImageGenerationToolOutput[]
+      )[0].result.length
     ).toBeGreaterThan(0);
-    expect(response.additional_kwargs.tool_outputs[0].size).toContain(
-      "1024x1024"
-    );
+    expect(
+      (
+        response.additional_kwargs.tool_outputs as ImageGenerationToolOutput[]
+      )[0].size
+    ).toContain("1024x1024");
   });
 });
